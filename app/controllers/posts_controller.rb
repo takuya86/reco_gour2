8cfb146841055019d6c0e341bb25904b67bb0578
takuyakawase <takuya86takuya@gmail.com>
2020-08-08 22:31:46 +0900

    Fix:フラッシュメッセージ表示位置修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @comments = @post.commentsbundle exe
+    @comments = @post.comments
     @comment = Comment.new
   end
 
